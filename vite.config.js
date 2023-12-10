db636d9f79fc43fe195dfef0cd5f59e4ecc3e588
Kyle Kilat <kkilat@users.noreply.github.com>
2023-12-10 12:29:56 -0800

    chore: create a more intuitive way to configure ENTRY_FILES for production builds

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,21 @@
 import { defineConfig } from 'vite';
 
+// Entry files (from `src`) to be bundled into separate output files (in `dist`) during production builds.
+// Add any others as objects with `inputPath` and `outputName` properties (output extensions are added automatically).
+const ENTRY_FILES = [
+  {
+    inputPath: './src/my-lib.js',
+    outputName: 'my-lib',
+  },
+];
+
 export default defineConfig({
   build: {
     lib: {
-      entry: {
-        'my-lib': './src/my-lib.js',
-      },
+      entry: ENTRY_FILES.reduce((entries, { inputPath, outputName }) => {
+        entries[outputName] = inputPath;
+        return entries;
+      }, {}),
       formats: ['es'],
     },
   },
