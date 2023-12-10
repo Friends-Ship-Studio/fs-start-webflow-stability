4b3b1e1ec50538e7cb04bd519552b66b0a61560a
Kyle Kilat <kkilat@users.noreply.github.com>
2023-12-10 10:59:59 -0800

    chore: add comment + placeholder code for window.Webflow wrapper

--- src/my-lib.js
+++ src/my-lib.js
@@ -1,5 +1,12 @@
+// If custom code relies on the Webflow.js library, or jQuery loaded by Webflow,
+// it should be wrapped in a function that is pushed to the Webflow global array.
+// This ensures that the code will be run AFTER the Webflow provided libraries.
+// --------
+// window.Webflow ||= [];
+// window.Webflow.push(printHelloWorld);
+
+printHelloWorld();
+
 function printHelloWorld() {
   console.log('Hello world!');
 }
-
-printHelloWorld();
