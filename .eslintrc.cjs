20fce0e9aeac0e479d2f1f2464fa81caad16997d
Kyle Kilat <kkilat@users.noreply.github.com>
2023-12-09 20:08:33 -0800

    chore: add and run ESLint for code linting

---
+++ .eslintrc.cjs
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const path = require('path');
+
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    // Default to the recommended ESLint rules
+    // Change or include additional rulesets here
+    'eslint:recommended',
+    // Remove ESLint rules that conflict with Prettier (must be last in the extends array)
+    'prettier',
+  ],
+  ignorePatterns: [
+    // Load .gitignore and .prettierignore content as base ESLint ignore patterns
+    ...fs
+      .readFileSync(path.join(__dirname, '.gitignore'), 'utf8')
+      .split('\n')
+      .filter((line) => line && !line.startsWith('#')),
+    ...fs
+      .readFileSync(path.join(__dirname, '.prettierignore'), 'utf8')
+      .split('\n')
+      .filter((line) => line && !line.startsWith('#')),
+    // Include any additional ignore patterns here
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {},
+};
